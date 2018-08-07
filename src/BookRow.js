8cb6da6949d411979498f1f35de5fa9b0a69c77e
Octowl <alsaff1987@gmail.com>
2018-08-07 16:18:49 +0300

    Make authors links

--- src/BookRow.js
+++ src/BookRow.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class BookRow extends Component {
   render() {
     const book = this.props.book;
+    const authors = book.authors.map(author => (
+      <div key={author.name}>
+        <Link to={`/authors/${author.id}`}>{author.name}</Link>
+      </div>
+    ));
     return (
       <tr>
         <td>{book.title}</td>
-        <td>
-          {book.authors.map(author => (
-            <div key={author.name}>{author.name}</div>
-          ))}
-        </td>
+        <td>{authors}</td>
         <td>
           <button className="btn" style={{ backgroundColor: book.color }} />
         </td>
