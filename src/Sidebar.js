82c0fa964da649edae92a9067d3184899da80b94
Octowl <alsaff1987@gmail.com>
2018-08-12 14:56:10 +0300

    Import logo directly

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+// Logo
+import logo from "./assets/theindex.svg";
+
 class Sidebar extends Component {
   render() {
     return (
       <div id="sidebar">
-        <img src="theindex.svg" className="logo" alt="the index logo" />
+        <img src={logo} className="logo" alt="the index logo" />
         <section>
           <h4 className="menu-item active">
             <NavLink to="/authors">AUTHORS</NavLink>
